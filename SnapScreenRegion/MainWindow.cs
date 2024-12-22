76f7b7e2e42bb5e7e30f4026acc37001a6f6e0e4
teralox <mikhailsafonov1985@gmail.com>
2024-12-22 23:49:11 +0100

    First Commit
    still needs more work, region not set yet

---
+++ SnapScreenRegion/MainWindow.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Atk;
+using Gtk;
+using SnapScreenRegion;
+using System.Threading.Tasks;
+using System.Threading;
+
+public partial class MainWindow : Gtk.Window
+{
+    SnapScreenReg snapScren;
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+
+        snapScren = new SnapScreenReg();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnSnapScreenButtonClicked(object sender, EventArgs e)
+    {
+        SnapScreenButton.ModifyBg(Gtk.StateType.Normal ,new Gdk.Color(200, 1, 1));
+        SnapScreenButton.ModifyFg(Gtk.StateType.Normal, new Gdk.Color(200, 1, 1));
+        TopLabel.ModifyFg(Gtk.StateType.Normal, new Gdk.Color(200, 23, 54));
+
+        this.Hide();
+
+        
+
+        //await Task.Delay(1000);
+        Thread.Sleep(1000); 
+
+        snapScren.SnapShot(Gdk.Global.DefaultRootWindow);
+
+        //await Task.Delay(1000);
+
+        this.Show();
+
+
+        // Remove From RAM
+
+        int ident = GC.GetGeneration(ImageCanvasPic);
+        GC.Collect(ident, GCCollectionMode.Forced);
+
+        // PUT IMAGE IN A CANVAS
+
+        ImageCanvasPic.Pixbuf = snapScren.PixBufImage;
+        ImageCanvasPic.Show();
+
+        snapScren.PutImageInWindow(Gdk.Global.DefaultRootWindow);
+    }
+
+    //protected async void OnSnapScreenButtonClicked(object sender, EventArgs e)
+    //{
+    //    SnapScreenButton.ModifyBg(Gtk.StateType.Normal, new Gdk.Color(200, 1, 1));
+    //    SnapScreenButton.ModifyFg(Gtk.StateType.Normal, new Gdk.Color(200, 1, 1));
+    //    TopLabel.ModifyFg(Gtk.StateType.Normal, new Gdk.Color(200, 23, 54));
+
+    //    this.Hide();
+
+
+
+    //    //await Task.Delay(1000);
+    //    Thread.Sleep(1000);
+
+    //    await snapScren.SnapShot(Gdk.Global.DefaultRootWindow);
+
+    //    //await Task.Delay(1000);
+
+    //    this.Show();
+
+
+    //    // Remove From RAM
+
+    //    int ident = GC.GetGeneration(ImageCanvasPic);
+    //    GC.Collect(ident, GCCollectionMode.Forced);
+
+    //    // PUT IMAGE IN A CANVAS
+
+    //    ImageCanvasPic.Pixbuf = snapScren.PixBufImage;
+    //    ImageCanvasPic.Show();
+
+    //    await snapScren.PutImageInWindow(Gdk.Global.DefaultRootWindow);
+    //}
+
+    //protected void OnMotionNotifyEvent(object o, MotionNotifyEventArgs args)
+    //{
+    //    TopLabel.Text = args.Event.X.ToString();
+    //}
+}
---
+++ SnapScreenRegion/SnapScreenReg.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Cairo;
+using Gtk;
+
+namespace SnapScreenRegion
+{
+    public class SnapScreenReg
+    {
+        public SnapScreenReg()
+        {
+        }
+
+        public void SnapShot(Gdk.Window window)
+        {
+            Gdk.Pixbuf pixBuf = new Gdk.Pixbuf(Gdk.Colorspace.Rgb, false, 8,
+                                   window.Screen.Width, window.Screen.Height);
+            pixBuf.GetFromDrawable(window, Gdk.Colormap.System, 0, 0, 0, 0,
+                                   window.Screen.Width, window.Screen.Height);
+            PixBufImage = pixBuf;
+            //pixBuf.ScaleSimple(400, 300, Gdk.InterpType.Hyper);
+            //pixBuf.Save("/home/mike/Desktop/screenshot0.jpeg", "jpeg");
+            //pixBuf.Savev("/home/mike/Desktop/screenshot0_" + DateTime.Now.Second + ".jpeg", "jpeg", new string[] { "quality" }, new string[] { "10" });
+
+        }
+
+        public void SnapShot2(Gdk.Window window)
+        {
+            Gdk.Pixbuf pixBuf = new Gdk.Pixbuf(Gdk.Colorspace.Rgb, false, 8,
+                                       window.Screen.Width, window.Screen.Height);
+            pixBuf.GetFromDrawable(window, Gdk.Colormap.System, 0, 0, 0, 0,
+                                   window.Screen.Width, window.Screen.Height);
+            pixBuf.ScaleSimple(400, 300, Gdk.InterpType.Hyper);
+            //pixBuf.Save("/home/mike/Desktop/screenshot0.jpeg", "jpeg");
+            pixBuf.Savev("/home/mike/Desktop/screenshot0_" + DateTime.Now.Second + ".jpeg", "jpeg", new string[] { "quality" }, new string[] { "100" });
+
+        }
+
+        public void PutImageInWindow(Gdk.Window window)
+        {
+            double xSize = (double)window.Screen.Width * 0.8;
+            double ySize = (double)window.Screen.Height * 0.8;
+
+            Window myWin = new Window("");
+            myWin.Resize((int)xSize, (int)ySize);
+
+            Image image = new Image();
+            myWin.Add(image);
+            image.Pixbuf = PixBufImage;
+            //image.Get(pixbuf.GetFr, null);
+
+            myWin.Show();
+            image.Show();
+        }
+
+        //public async Task SnapShot(Gdk.Window window)
+        //{
+        //    try
+        //    {
+        //        await Task.Run(() =>
+        //        {
+        //            Gdk.Pixbuf pixBuf = new Gdk.Pixbuf(Gdk.Colorspace.Rgb, false, 8,
+        //                                   window.Screen.Width, window.Screen.Height);
+        //            pixBuf.GetFromDrawable(window, Gdk.Colormap.System, 0, 0, 0, 0,
+        //                                   window.Screen.Width, window.Screen.Height);
+        //            PixBufImage = pixBuf;
+        //            //pixBuf.ScaleSimple(400, 300, Gdk.InterpType.Hyper);
+        //            //pixBuf.Save("/home/mike/Desktop/screenshot0.jpeg", "jpeg");
+        //            //pixBuf.Savev("/home/mike/Desktop/screenshot0_" + DateTime.Now.Second + ".jpeg", "jpeg", new string[] { "quality" }, new string[] { "10" });
+        //        });
+        //    }
+        //    catch (Exception excp)
+        //    {
+        //        Console.WriteLine(excp.ToString() + " ...issue");
+        //    }
+        //    //Image image;
+
+        //}
+
+        //public async Task PutImageInWindow(Gdk.Window window)
+        //{
+        //    await Task.Run(() =>
+        //    {
+        //        double xSize = (double)window.Screen.Width * 0.8;
+        //        double ySize = (double)window.Screen.Height * 0.8;
+
+        //        Window myWin = new Window("");
+        //        myWin.Resize((int)xSize, (int)ySize);
+
+        //        Image image = new Image();
+        //        myWin.Add(image);
+        //        image.Pixbuf = PixBufImage;
+        //        //image.Get(pixbuf.GetFr, null);
+
+        //        myWin.Show();
+        //        image.Show();
+        //    });
+        //}
+
+        public Gdk.Pixbuf PixBufImage { get; set; }
+    }
+}
